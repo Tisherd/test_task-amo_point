d42dc2260296c42a6f0aadfc5c66176e130706a7
Tisherd <mtisherd@gmail.com>
2024-05-24 15:47:08 +0800

    correct use sqlite

--- env.php
+++ env.php
@@ -4,5 +4,5 @@ const ENV = [
     'files_storage_dir' => __DIR__ . '/files/',
     'max_allow_file_size' => 500000,
 
-    'sqlite_db_path' => __DIR__ . '/database/sqlite.db',
+    'sqlite_db_path' => __DIR__ . '/database/db.sqlite',
 ];
--- src/Database/SqliteConnect.php
+++ src/Database/SqliteConnect.php
@@ -2,12 +2,17 @@
 
 namespace Src\Database;
 
+use SQLite3;
+
 class SqliteConnect
 {
     private static $connect;
 
     public static function connect() {
         if (self::$connect == null) {
+            if (!file_exists(ENV['sqlite_db_path'])) {
+                new SQLite3(ENV['sqlite_db_path']);
+            }
             self::$connect = new \PDO(dsn: "sqlite:" . ENV['sqlite_db_path']);
             self::$connect->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             self::$connect->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
